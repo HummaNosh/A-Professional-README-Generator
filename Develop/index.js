0d2380e968a5d862f87211f78ebb93032b0a8f85
Humma Noshine <Hummanoshine@gmail.com>
2022-02-23 01:10:50 +0000

    test2

--- Develop/index.js
+++ Develop/index.js
@@ -137,7 +137,8 @@ You can also visit my Github profile here www.github.com/${github}
 function getLicenseBadge() {
   switch (choices.license) {
     case ["Apache 2.0"]:
-      Badge = "(https://opensource.org/licenses/Apache-2.0)";
+      Badge =
+        "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
     case "MIT":
       Badge =
         "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
